ff42130b777c4815932dda8befdbca434351db36
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-09-15 22:20:31 +0200

    corrected default output folder for mesh export

--- src/FileDialog.py
+++ src/FileDialog.py
@@ -1,4 +1,6 @@
 
+import os
+
 from Settings import DIALOGFILTER, OUTPUTDATA, AIRFOILDATA
 
 from PySide6 import QtWidgets
@@ -23,10 +25,11 @@ class Dialog:
             string: filename inlcuding path to filename
             string: filter which was selected
         """
+        path = os.path.join(OUTPUTDATA, filename)
         filename, selected_filter = QtWidgets.QFileDialog.getSaveFileName(
             None,
             'Save File As',
-            OUTPUTDATA + filename,
+            path,
             self.filter,
             '')
 
