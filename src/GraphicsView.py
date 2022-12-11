807089fa28a511690adf2717fae14f3199a1dd35
Andreas Ennemoser <andreas.ennemoser@aon.at>
2022-12-11 09:38:50 +0100

    testing

--- src/GraphicsView.py
+++ src/GraphicsView.py
@@ -331,6 +331,7 @@ class GraphicsView(QtWidgets.QGraphicsView):
         # 
         current_zoom = self.transform().m11()
         scale_marker = 1. + 3. * (current_zoom - MINZOOM) / (MAXZOOM - MINZOOM)
+        # scale_marker = 100.
         logger.info(f'Current zoom value {current_zoom}')
         logger.info(f'Scale factor for markers {scale_marker}')
 
