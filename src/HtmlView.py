f00ba2f2b06208ca0633376d25706d0318eb5118
Andreas Ennemoser <andreas.ennemoser@aon.at>
2024-12-14 13:06:18 +0100

    Remove unused HtmlView class and its dependencies

--- src/HtmlView.py
+++
@@ -1,8 +0,0 @@
-from PySide6 import QtGui, QtCore
-
-
-class HtmlView(QtGui.QWebView):
-    """docstring for HtmLvIEW"""
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
